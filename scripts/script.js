b1b3579971fcdbdd0fca3228641ecc074d7bf0a4
nutenjoyer <111534264+nutenjoyer@users.noreply.github.com>
2022-12-01 14:11:07 -0600

    console box attempt 2

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,12 @@
 $(function(){
     $('#console-box').append('console is ready...');
-
     
+    $('li').click(function(){
+      $('#console-box').append('<br>class:'+$(this).attr("class")+'|value:'+$(this).text()+'.');
+      $(this).toggleClass('selected').siblings().removeClass('selected');
+  ; 
+    });
+    
+  });
+    
+  
